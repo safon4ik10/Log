ba4522b85a05de478575be7dc81aaa1d6318b9fb
vtroshin <vtroshin@rvision.ru>
2022-10-11 11:02:34 +0500

    Init commit

---
+++ Filter.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filter {
+    protected int treshold;
+
+    public Filter(int treshold) {
+        this.treshold = treshold;
+    }
+
+    public List<Integer> filterOut(List<Integer> source) {
+        Logger logger = Logger.getInstance();
+        List<Integer> result = new ArrayList<>();
+        for (Integer i : source){
+            if (i < treshold){
+                logger.log("Элемент " + i + " не прошел");
+            } else {
+                result.add(i);
+                logger.log("Элемент " + i + " прошел");
+            }
+        }
+        logger.log("Прошло фильтр " + result.size() + " элемента из " + source.size());
+        logger.log("Выводим рузльтат на экран");
+        System.out.print("Отфильтрованный список ");
+        result.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+        return result;
+    }
+}
---
+++ Logger.java
@@ -0,0 +1,28 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+    private static Logger logger;
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
+    protected int num = 1;
+
+    private Logger(){
+
+    }
+
+    public static Logger getInstance(){
+        if (logger == null){
+            return new Logger();
+        }
+        return logger;
+    }
+
+    public void log(String msg){
+        System.out.println("[" + sdf.format(new Date().getTime()) + " " + num++ + "] " + msg);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
